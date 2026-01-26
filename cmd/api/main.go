de71efc08b60a6e2f511014e1303c8e87bed7a71
PedroNieto98 <pedro@creatorsa2z.com>
2026-01-26 10:10:26 -0300

    add headers cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"math/rand"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -102,7 +103,7 @@ func initializeRunner(ctx context.Context, sessionService session.Service, artif
 
 func setupRouter(ctx context.Context, sessionUseCase *usecase.SessionUseCase, userUseCase *usecase.UserUseCase, feedbackUseCase *usecase.EventFeedbackUseCase, runn *runner.Runner) *gin.Engine {
 	r := gin.Default()
-
+	r.Use(cors.Default())
 	r.Use(middleware.AuthMiddleware(*userUseCase))
 
 	sessionHandler := handler.NewSessionHandler(sessionUseCase)
